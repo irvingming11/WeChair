2964daea2f97d0882677ddbf78d997de5c54b957
irvingming11 <irvingming11@sina.com>
2020-05-01 11:25:33 +0800

    first commit test

---
+++ src/test/java/com/at/wechair/WeChairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.at.wechair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeChairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
