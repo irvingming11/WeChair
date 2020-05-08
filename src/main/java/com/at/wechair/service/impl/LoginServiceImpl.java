f7650c66bb6b5bd001dc454f1bf589c9abc9e820
irvingming11 <irvingming11@sina.com>
2020-05-08 11:04:44 +0800

    对解密失败的情况添加异常处理和数据库事务回滚

--- src/main/java/com/at/wechair/service/impl/LoginServiceImpl.java
+++ src/main/java/com/at/wechair/service/impl/LoginServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
